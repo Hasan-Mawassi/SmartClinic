ffd5bac4d90d98c14688f30ec85bd8968b45780e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-05 00:16:06 +0300

    remove chatbot test api

--- SmartClinic-Server/App.js
+++ SmartClinic-Server/App.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import env from 'dotenv';
 import Routes from './Routes/route.js';
-import { chatWithBot } from './openAi/assistance.js';
 const app = express();
 env.config();
 app.use(express.json()); 
 
 app.use(Routes)
-app.post('/ai/chat',chatWithBot)
+
 // Route to test if it's running
 app.get('/', (req, res) => {
   res.send('Hello from the server !!!!');
