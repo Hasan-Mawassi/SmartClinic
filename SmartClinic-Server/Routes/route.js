d3f1f98af2b59c0c97a5d8a117a314353a0073c9
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-05 00:15:28 +0300

    add chatbot router with its prefix

--- SmartClinic-Server/Routes/route.js
+++ SmartClinic-Server/Routes/route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import AuthRouter from "./authRoute.js";
-
+import chatRouter from "./chatBotRoute.js";
 const Routes = Router();
 
 Routes.use('/api/v1/auth',AuthRouter);
-
+Routes.use('/api/v1/ai',chatRouter);
 export default Routes;
