673222a2717b4eef2c0cc324132953984b0657a1
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-05 20:49:06 +0300

    add voice controller

--- SmartClinic-Server/Controllers/chatbotController.js
+++ SmartClinic-Server/Controllers/chatbotController.js
@@ -1,5 +1,5 @@
 import { chatWithBot } from "../openAi/assistance.js";
-
+import { transcribeAudio } from "../Services/openAi/voiceService.js";
 
 export const textChatBot = async (req, res) => {
     try {
@@ -11,3 +11,19 @@ export const textChatBot = async (req, res) => {
         res.status(error.status || 500).json({ error: error.message || 'Internal server error' });
     }
     }
+
+    export const voiceTranscribe =   async (req, res) => {
+        try { 
+          const { audioBase64, fileType, userName, doctor } = req.body;
+      
+          if (!audioBase64 || !fileType) {
+            return res.status(400).json({ error: 'Missing audioBase64 or fileType' });
+          }
+        const textFromAudio= await  transcribeAudio(audioBase64, fileType);
+        const result = await chatWithBot(userName, textFromAudio, doctor);
+          res.status(200).json(result);
+        } catch (err) {
+          console.error('Error in base64 transcription:', err);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      };
