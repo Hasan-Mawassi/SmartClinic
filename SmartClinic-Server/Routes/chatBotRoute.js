6903f3ba09baa94ca62aca8ef2df30dc5bf16fc5
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-05 20:49:34 +0300

    add voice end point

--- SmartClinic-Server/Routes/chatBotRoute.js
+++ SmartClinic-Server/Routes/chatBotRoute.js
@@ -4,5 +4,6 @@ import * as chatBotContrller from "../Controllers/chatbotController.js";
 const chatRouter = Router();
 
 chatRouter.post("/chat", chatBotContrller.textChatBot);
+chatRouter.post("/voice", chatBotContrller.voiceTranscribe);
 
 export default chatRouter;
