ea247e70f5d87ed662e4fd26785fa104a1c2bfe7
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-05 23:38:09 +0300

    add response trait for chatbot Controller

--- SmartClinic-Server/Controllers/chatbotController.js
+++ SmartClinic-Server/Controllers/chatbotController.js
@@ -1,29 +1,30 @@
 import { chatWithBot } from "../openAi/assistance.js";
 import { transcribeAudio } from "../Services/openAi/voiceService.js";
-
+import { successResponse ,errorResponse } from "../Traits/response.js";
 export const textChatBot = async (req, res) => {
     try {
         const { userName, message, doctor} = req.body;
         const result = await chatWithBot(userName, message, doctor);
-        res.status(200).json(result);
+        successResponse(res, result, 'Chatbot response', 200);
+        // res.status(200).json(result);
     } catch (error) {
         console.error('Chatbot error:', error.message);
-        res.status(error.status || 500).json({ error: error.message || 'Internal server error' });
+        errorResponse(res, error, 500);
+        // res.status(error.status || 500).json({ error: error.message || 'Internal server error' });
     }
     }
 
     export const voiceTranscribe =   async (req, res) => {
         try { 
           const { audioBase64, fileType, userName, doctor } = req.body;
-      
-          if (!audioBase64 || !fileType) {
-            return res.status(400).json({ error: 'Missing audioBase64 or fileType' });
-          }
+
         const textFromAudio= await  transcribeAudio(audioBase64, fileType);
         const result = await chatWithBot(userName, textFromAudio, doctor);
-          res.status(200).json(result);
+        successResponse(res, result, 'Chatbot response', 200);
+        //   res.status(200).json(result);
         } catch (err) {
           console.error('Error in base64 transcription:', err);
-          res.status(500).json({ error: 'Internal server error' });
+          errorResponse(res, err, 500);
+        //   res.status(500).json({ error: 'Internal server error' });
         }
       };
