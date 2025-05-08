0f5d63d20a645b2199582dc3ba5608a21818ec69
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-08 21:07:29 +0300

    add voice provider

--- SmartClinic-Mobile/patient_app/lib/providers/chat_provider.dart
+++ SmartClinic-Mobile/patient_app/lib/providers/chat_provider.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:patient_app/utils/request.dart';
+import 'package:patient_app/utils/handle_ai_response.dart';
 
 import '../models/messages.dart';
 
@@ -18,10 +19,16 @@ class ChatProvider with ChangeNotifier {
     "slotDuration": 30,
   };
 
-  void addUserMessage(String text) {
-    _messages.add(Message(text: text, sender: Sender.user));
+  void addUserMessage(String text, {bool isVoice = false}) {
+    final message = Message(
+      text: text,
+      sender: Sender.user,
+      type: isVoice ? MessageType.voice : MessageType.text,
+    );
+    _messages.add(message);
     notifyListeners();
-    _sendToApi(text);
+    isVoice ? sendVoiceMessage(text) : _sendToApi(text);
+    // _sendToApi(text);
   }
 
   Future<void> _sendToApi(String userInput) async {
@@ -31,62 +38,8 @@ class ChatProvider with ChangeNotifier {
         "ai/chat",
         data: {"userName": username, "message": userInput, "doctor": doctor},
       );
-
-      if (response.statusCode == 200) {
-        final data = response.data;
-        final rawMessage =
-            data['data']['message'] ?? 'Sorry, something went wrong.';
-
-        
-        try {
-          final parsedMessage = json.decode(rawMessage);
-
-          // Case: Available slots
-          if (parsedMessage['available_slots'] != null) {
-            final date = parsedMessage['date'];
-            final prompt = parsedMessage['message'];
-            
-            final slots = parsedMessage['available_slots'] as List<dynamic>;
-
-            final formatted =
-                '\n$prompt \n Available slots on $date:\n' +
-                slots.asMap().entries.map((e) => "${e.key + 1}. ${e.value}").join('\n');
-
-            _messages.add(Message(text: formatted, sender: Sender.ai));
-          }
-          // Case: Appointment confirmation
-          else if (parsedMessage['appointment_details'] != null) {
-            final details = parsedMessage['appointment_details'];
-            final date = details['date'];
-            final time = details['time'];
-            final status = details['status'];
-            final note = parsedMessage['message'] ?? "";
-
-            final formatted = '''
-              📅 Appointment Details:
-              - Date: $date
-              - Time: $time
-              - Status: $status
-
-              📝 Note: $note
-              ''';
-            _messages.add(Message(text: formatted, sender: Sender.ai));
-          } else {
-            // Unstructured or unknown format
-            _messages.add(Message(text: rawMessage, sender: Sender.ai));
-          }
-        } catch (e) {
-          // rawMessage is plain text
-          _messages.add(Message(text: rawMessage, sender: Sender.ai));
-        }
-      } else {
-        _messages.add(
-          Message(
-            text: "Error: ${response.statusCode} ${response.statusMessage}",
-            sender: Sender.ai,
-          ),
-        );
-      }
+     handleAIResponse(response, _messages);
+     
     } catch (e) {
       _messages.add(
         Message(text: "Failed to connect to server.", sender: Sender.ai),
@@ -98,22 +51,32 @@ class ChatProvider with ChangeNotifier {
 
   void sendVoiceMessage(String base64Audio) async {
     final requestClient = RequestClient().dio;
-  messages.add(
-    Message(sender: Sender.user, text: "[Voice message sent]"),
-  );
-  notifyListeners();
+    messages.add(Message(sender: Sender.user, text: "[Voice message sent]"));
+    notifyListeners();
 
-  // Send base64Audio to your API here
-  final response = await requestClient.post(
+    // Send base64Audio to your API here
+    try {
+      final response = await requestClient.post(
         "ai/voice",
-        data: {"userName": username, "message": base64Audio, "doctor": doctor, "fileType": "m4a"},
+        data: {
+          "userName": username,
+          "audioBase64": base64Audio,
+          "doctor": doctor,
+          "fileType": "m4a",
+        },
       );
-  // = await api.sendVoice(base64Audio);
+      print("Response: ${response.data}");
+       handleAIResponse(response, _messages);
+    } catch (e) {
+      _messages.add(
+        Message(text: "Failed to connect to server.", sender: Sender.ai),
+      );
+      print("Request failed: $e");
+    }
+    
 
-  messages.add(
-    Message(sender: Sender.ai, text: response.data['message']),
-  );
-  notifyListeners();
+    notifyListeners();
+  }
 }
 
-}
+
