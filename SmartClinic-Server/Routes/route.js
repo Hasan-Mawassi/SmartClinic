31e3a3a35b9499fa59958a652e4108c0a94e3f10
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-09 01:01:43 +0300

    add doctor router to routes

--- SmartClinic-Server/Routes/route.js
+++ SmartClinic-Server/Routes/route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import AuthRouter from "./authRoute.js";
 import chatRouter from "./chatBotRoute.js";
+import doctorRouter from "./doctorRoute.js";
 const Routes = Router();
 
 Routes.use('/api/v1/auth',AuthRouter);
 Routes.use('/api/v1/ai',chatRouter);
+Routes.use('/api/v1',doctorRouter);
 export default Routes;
