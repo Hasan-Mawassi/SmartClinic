4a8d31b1d1cf6df0131d3a924ba7a73230a97890
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-08 21:02:09 +0300

    add voice message widget

---
+++ SmartClinic-Mobile/patient_app/lib/widgets/chatbot/voice_message.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'dart:typed_data';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class VoiceMessageWidget extends StatefulWidget {
+  final String base64Audio;
+
+  const VoiceMessageWidget({Key? key, required this.base64Audio})
+    : super(key: key);
+
+  @override
+  _VoiceMessageWidgetState createState() => _VoiceMessageWidgetState();
+}
+
+class _VoiceMessageWidgetState extends State<VoiceMessageWidget> {
+  late FlutterSoundPlayer _audioPlayer;
+  bool _isPlaying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+      onPressed: _togglePlay,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initPlayer();
+  }
+
+  Future<void> _initPlayer() async {
+    _audioPlayer = FlutterSoundPlayer();
+    await _audioPlayer.openPlayer();
+  }
+
+  Future<void> _togglePlay() async {
+    if (_isPlaying) {
+      await _audioPlayer.stopPlayer();
+    } else {
+      Uint8List bytes = base64Decode(widget.base64Audio);
+      final dir = await getTemporaryDirectory();
+      File file = File('${dir.path}/temp_audio.mp4');
+      await file.writeAsBytes(bytes);
+
+      await _audioPlayer.startPlayer(
+        fromURI: file.path,
+       codec: Codec.aacMP4,
+        whenFinished: () => setState(() => _isPlaying = false),
+      );
+    }
+    setState(() => _isPlaying = !_isPlaying);
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.closePlayer();
+    super.dispose();
+  }
+}
