1e88d718fa2b0b7742d48e867749b93ba05bf21e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 16:28:32 +0300

    add login card

--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import './style.css';
 import InputField from '../../components/Basic/inputField';
 import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
 import CustomCard from '../../components/Basic/authCard';
 import CustomButton from '../../components/Basic/Button';
 const Auth = () => {
     return (
         <div className='auth-body'>
-            <div className='top-right-circle'></div>
-        <CustomCard title='Login'>
+        <div className='top-right-circle'></div>
+          <CustomCard title='Login'>
               <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
-              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
-              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+              <InputField inputLabel='Password' type="password" variant="outlined" fullWidth={true} icon={LockIcon} />
+             
               <CustomButton
                 className='register-button'
-                label="Register"
+                label="Login"
                 onClick={() => { 
                 }}
                 loading={false}
                 />
-        </CustomCard>
+           </CustomCard>
                     
                 <div className="bottom-left-circle"></div>
           
