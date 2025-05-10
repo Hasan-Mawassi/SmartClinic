3ea6080ab08821a9f7ebd1975b3123731546e831
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 16:43:12 +0300

    make login card

---
+++ SmartClinic-Web/doctor-app/src/components/Auth/LoginCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import InputField from '../../components/Basic/inputField';
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
+import CustomCard from '../../components/Basic/authCard';
+import CustomButton from '../../components/Basic/Button';
+import { Link } from '@mui/material';
+const LoginCard = ({linkClick , loginClick}) => {
+    return (
+        <CustomCard title='Login'>
+              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+              <InputField inputLabel='Password' type="password" variant="outlined" fullWidth={true} icon={LockIcon} />
+             
+              <CustomButton
+                className='register-button'
+                label="Login"
+                onClick={loginClick}
+                loading={false}
+                />
+               <div className='link'>
+               Create an account?{' '}
+                <Link
+                component="button"
+                variant="body2"
+                onClick={linkClick}
+                sx={{ textDecoration: 'underline', cursor: 'pointer' }}
+                >
+                Sign up
+                </Link>
+               </div>
+           </CustomCard>
+    );
+};
+
+export default LoginCard;
--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -1,27 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import InputField from '../../components/Basic/inputField';
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
-import PersonIcon from '@mui/icons-material/Person';
-import CustomCard from '../../components/Basic/authCard';
-import CustomButton from '../../components/Basic/Button';
+import LoginCard from '../../components/Auth/LoginCard';
 const Auth = () => {
+    const [isLogin, setIsLogin] = useState(true);
     return (
         <div className='auth-body'>
         <div className='top-right-circle'></div>
-          <CustomCard title='Login'>
-              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
-              <InputField inputLabel='Password' type="password" variant="outlined" fullWidth={true} icon={LockIcon} />
-             
-              <CustomButton
-                className='register-button'
-                label="Login"
-                onClick={() => { 
-                }}
-                loading={false}
-                />
-           </CustomCard>
+          {isLogin &&(<LoginCard linkClick={() => setIsLogin(false)} loginClick={() => console.log('login clicked')}/>)}
                     
                 <div className="bottom-left-circle"></div>
           
