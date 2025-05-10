8cf28aa9277208a2b4ecc2b3a1bd9436f26fe31d
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 15:44:37 +0300

    add custom button

--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -3,6 +3,7 @@ import './style.css';
 import InputField from '../../components/Basic/inputField';
 import EmailIcon from '@mui/icons-material/Email';
 import CustomCard from '../../components/Basic/authCard';
+import CustomButton from '../../components/Basic/Button';
 const Auth = () => {
     return (
         <div className='auth-body'>
@@ -11,6 +12,13 @@ const Auth = () => {
               <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
               <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
               <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+              <CustomButton
+                className='register-button'
+                label="Register"
+                onClick={() => { 
+                }}
+                loading={false}
+                />
         </CustomCard>
                     
 
