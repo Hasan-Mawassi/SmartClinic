682becb03a86ef3e25d2a8ef6d806ae5fd1bf97e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 13:48:21 +0300

    add react router

--- SmartClinic-Web/doctor-app/src/App.jsx
+++ SmartClinic-Web/doctor-app/src/App.jsx
@@ -1,14 +1,14 @@
-
 import './App.css'
-
+import Auth from './pages/auth'
+import { Route, Routes} from 'react-router-dom'
 function App() {
 
   
 
   return (
-    <>
-      
-    </>
+    <Routes>
+    <Route path="/auth" element={<Auth />} />
+  </Routes>
   )
 }
 
