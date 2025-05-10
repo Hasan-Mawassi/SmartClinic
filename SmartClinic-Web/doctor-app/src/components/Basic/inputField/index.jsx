cb5d9a215c925aaa1cf717747675f37bdd8c5021
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 14:34:10 +0300

    add icon to input field

--- SmartClinic-Web/doctor-app/src/components/Basic/inputField/index.jsx
+++ SmartClinic-Web/doctor-app/src/components/Basic/inputField/index.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 import './style.css'
 const inputField = ({
     id = 'custom-textfield',
-    label,
     value,
+    inputLabel,
     onChange,
     variant = 'outlined',
     type = 'text',
+    icon: Icon,
     fullWidth = true,
     ...props
   }) => {
 
     return (
+        <div className='input-field-container'>
+        <label htmlFor={id} className='input-label'>{inputLabel} </label>
         <TextField
         className='input-field'
         id={id}
-        label={label}
         variant={variant}
         value={value}
         onChange={onChange}
         type={type}
         fullWidth={fullWidth}
+        InputProps={{
+            startAdornment: Icon ? (
+              <InputAdornment position="start">
+                <Icon />
+              </InputAdornment>
+            ) : null,
+          }}
         {...props}
       />
+      </div>
     );
 
 };
--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import InputField from '../../components/Basic/inputField';
+import EmailIcon from '@mui/icons-material/Email';
 const Auth = () => {
     return (
         <div>
-           <InputField label="Email" type="email" variant="outlined" fullWidth={true} />
+           <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
         </div>
     );
 };
