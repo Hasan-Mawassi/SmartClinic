5c2eaf15eabd8b1448c4cfefe4d060bf4f3b6dfb
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 15:08:33 +0300

    add auth card and fix page padding

--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './style.css';
 import InputField from '../../components/Basic/inputField';
 import EmailIcon from '@mui/icons-material/Email';
+import CustomCard from '../../components/Basic/authCard';
 const Auth = () => {
     return (
-        <div>
-           <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+        <div className='auth-body'>
+            
+        <CustomCard title='Login'>
+              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+              <InputField inputLabel='Email' type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
+        </CustomCard>
+                    
+
+          
         </div>
     );
 };
