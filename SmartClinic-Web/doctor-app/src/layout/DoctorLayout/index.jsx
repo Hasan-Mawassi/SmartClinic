ea098fd4dc70271fc7562d7d476396d2b5ce659a
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-10 21:29:07 +0300

    create side bar

--- SmartClinic-Web/doctor-app/src/layout/DoctorLayout/index.jsx
+++ SmartClinic-Web/doctor-app/src/layout/DoctorLayout/index.jsx
@@ -1,12 +1,22 @@
 // components/Layout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import Sidebar from '../../components/common/SideBar';
+import { Box, CssBaseline, Toolbar } from '@mui/material';
 
 const Layout = () => {
   return (
-   
-          <Outlet />
+    <>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Sidebar />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* <Toolbar /> */}
+        <Outlet />
+      </Box>
+    </Box>
+    </>
+      
       
   );
 };
