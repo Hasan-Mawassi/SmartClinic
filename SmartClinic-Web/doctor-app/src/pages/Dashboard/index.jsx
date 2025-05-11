33a652cf466a45b7250f47f805098f4a1f36437a
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-11 12:21:57 +0300

    add kpi to the dashboard

--- SmartClinic-Web/doctor-app/src/pages/Dashboard/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/Dashboard/index.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+// pages/Dashboard.jsx
+import React, { useEffect, useState } from 'react';
+import { Grid2,Box, CircularProgress } from '@mui/material';
+import KpiCard from '../../components/Basic/KPICard';
+
+import PeopleIcon from '@mui/icons-material/People';
+import EventIcon from '@mui/icons-material/Event';
+import ReportIcon from '@mui/icons-material/Description';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
 
 const Dashboard = () => {
+  const [kpis, setKpis] = useState(null);
+
+  useEffect(() => {
+   
+    setTimeout(() => {
+      setKpis({
+        totalPatients: 124,
+        upcomingAppointments: 8,
+        pendingReports: 5,
+        completedConsultations: 42
+      });
+    }, 800);
+  }, []);
+
+  if (!kpis) {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+      <Box display="flex" justifyContent="center" mt={5}>
+        <CircularProgress />
+      </Box>
     );
+  }
+
+  return (
+    <Box p={3}>
+      <Grid2 container spacing={3}>
+    <Grid2 xs={12} sm={6} md={3}>
+      <KpiCard
+        title="Total Patients"
+        value={kpis.totalPatients}
+        icon={<PeopleIcon fontSize="large" color="primary" />}
+      />
+    </Grid2>
+    <Grid2 xs={12} sm={6} md={3}>
+      <KpiCard
+        title="Upcoming Appointments"
+        value={kpis.upcomingAppointments}
+        icon={<EventIcon fontSize="large" color="secondary" />}
+      />
+    </Grid2>
+    <Grid2 xs={12} sm={6} md={3}>
+      <KpiCard
+        title="Pending Reports"
+        value={kpis.pendingReports}
+        icon={<ReportIcon fontSize="large" sx={{ color: '#fb8c00' }} />}
+      />
+    </Grid2>
+    <Grid2 xs={12} sm={6} md={3}>
+      <KpiCard
+        title="Completed This Month"
+        value={kpis.completedConsultations}
+        icon={<DoneAllIcon fontSize="large" sx={{ color: '#4caf50' }} />}
+      />
+    </Grid2>
+  </Grid2>
+    </Box>
+  );
 };
 
 export default Dashboard;
