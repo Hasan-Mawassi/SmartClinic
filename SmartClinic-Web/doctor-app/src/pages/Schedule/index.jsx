9779318b5c747eb20855545eacc1ebf81b3c0089
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-12 01:07:53 +0300

    add card and table for schedule page

--- SmartClinic-Web/doctor-app/src/pages/Schedule/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/Schedule/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ScheduleCard from '../../components/Schedule/ScheduleCard';
+import ScheduleTable from '../../components/Schedule/ScheduleTable';
+import { Box } from '@mui/material';
 const Schedule = () => {
     return (
-        <div>
+        <> 
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center',mt: 2 }}>
             <ScheduleCard />
-        </div>
+        </Box>
+        <Box sx={{p:1 }}>
+        < ScheduleTable />
+        </Box>
+        </>
     );
 };
 
