3553b0ea00fe4c939e4e577e6f176bd0b445b281
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-11 23:55:59 +0300

    add schedule card

---
+++ SmartClinic-Web/doctor-app/src/pages/Schedule/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ScheduleCard from '../../components/Schedule/ScheduleCard';
+const Schedule = () => {
+    return (
+        <div>
+            <ScheduleCard />
+        </div>
+    );
+};
+
+export default Schedule;
