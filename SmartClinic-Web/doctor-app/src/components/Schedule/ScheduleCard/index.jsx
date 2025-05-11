85c5a978c6a205fb885e52ae47fb62c81170d409
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-12 01:07:02 +0300

    add button for shedule card

--- SmartClinic-Web/doctor-app/src/components/Schedule/ScheduleCard/index.jsx
+++ SmartClinic-Web/doctor-app/src/components/Schedule/ScheduleCard/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import CustomCard from '../../Basic/authCard';
 import InputField from '../../Basic/inputField';
+import CustomButton from '../../Basic/Button';
 const SheduleCard = () => {
     return (
-        <CustomCard title='Login'>
+        <CustomCard title='Schedule'>
             <InputField inputLabel='Start Time' placeholder="Enter Start Time" type="text" variant="outlined" fullWidth={true}/>
             <InputField inputLabel='End Time' placeholder="Enter End Time" type="text" variant="outlined" fullWidth={true} />
             <InputField inputLabel='Slot Duration' placeholder="Enter Slot Duration in Min" type="text" variant="outlined" fullWidth={true} />
             <InputField inputLabel='Off Day' placeholder="Enter Off Day" type="text" variant="outlined" fullWidth={true} />
+            < CustomButton label={'Save'} sx={{alignSelf:"center", fontSize:'18px'}}/>
         </CustomCard>
     );
 };
