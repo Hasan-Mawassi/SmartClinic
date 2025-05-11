dce29d9041e67982257edca94afc6800aafe8f15
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-11 16:20:26 +0300

    add patient card to patient page

--- SmartClinic-Web/doctor-app/src/pages/Patient/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/Patient/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import PatientCard from '../../components/Patient/PatientCard'
 const Patient = () => {
+    const patient = {
+        name: 'John Doe',
+        age: 45,
+        weight: 72,
+        gender: 'Male',
+        bloodGroup: 'O+',
+        image: '/logo.png',
+        surgeryName: 'Appendectomy surgery',
+        surgeryDate: '2024-08-15'
+      };
+      
     return (
         <div>
             <h1>patient</h1>
+            <PatientCard patient={patient} />
         </div>
     );
 };
