8ccc558d8cdff68d0db0a1891d63b7ea8021ceb9
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 00:23:59 +0300

    add kpi end point

--- SmartClinic-Server/Routes/doctorRoute.js
+++ SmartClinic-Server/Routes/doctorRoute.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import * as doctorController from "../Controllers/doctorController.js";
+import * as dashboardController from "../Controllers/Doctor/dashboardController.js";
 const doctorRouter = Router();
 
 doctorRouter.get("/app/getdoctors", doctorController.getAllDoctors);
-
+doctorRouter.get("/doctor/getkpi/:id",dashboardController.kpiData )
 export default doctorRouter;
