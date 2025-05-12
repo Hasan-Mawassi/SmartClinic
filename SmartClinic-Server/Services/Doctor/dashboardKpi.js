761202edc400669c35bd5543c12337e7b04ea469
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 00:22:33 +0300

    update kpi sevice

--- SmartClinic-Server/Services/Doctor/dashboardKpi.js
+++ SmartClinic-Server/Services/Doctor/dashboardKpi.js
@@ -2,16 +2,16 @@ import prisma from '../../lib/prisma.js';
 import { startOfDay, endOfDay } from 'date-fns';
 import { Appointment } from '../../Models/Appointment.js';
 
-export const getDoctorKPIsService = async (doctorId) => {
+export const DoctorKPIsService = async (doctorId) => {
     const totalPatients = await Appointment.totalPatients(doctorId)
-  
+    const numberOfPateint = totalPatients.length;
     const upcomingAppointments = await Appointment.upcoming(doctorId)
     const todayStart = startOfDay(new Date());
     const todayEnd = endOfDay(new Date());
     const completedToday = await Appointment.completed(todayStart,todayEnd ,doctorId)
   
     return {
-      totalPatients,
+      totalPatients:numberOfPateint,
       upcomingAppointments,
       completedToday,
     };
