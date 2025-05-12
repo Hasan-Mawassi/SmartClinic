d614640232f125ed6c666941e9127488165937f9
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 00:51:05 +0300

    add monthly patient graph sevice

---
+++ SmartClinic-Server/Services/Doctor/graphData.js
@@ -0,0 +1,30 @@
+import { startOfMonth, endOfMonth } from 'date-fns';
+import prisma from '../../lib/prisma.js';
+import { months } from '../../utils/months.js';
+
+export const getMonthlyPatientsData = async (doctorId) => {
+  const currentYear = new Date().getFullYear();
+  const monthlyData = [];
+
+  for (const { label, number } of months) {
+    const start = startOfMonth(new Date(currentYear, number));
+    const end = endOfMonth(new Date(currentYear, number));
+
+    const count = await prisma.appointment.count({
+      where: {
+        doctorId,
+        dateTime: {
+          gte: start,
+          lte: end,
+        },
+      },
+    });
+
+    monthlyData.push({
+      month: label,
+      patients: count,
+    });
+  }
+
+  return monthlyData;
+};
