e694631533d04794485ef284fd7e1e10b8b5585c
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 00:52:23 +0300

    add graphs data controller

--- SmartClinic-Server/Controllers/Doctor/dashboardController.js
+++ SmartClinic-Server/Controllers/Doctor/dashboardController.js
@@ -1,5 +1,7 @@
 import { DoctorKPIsService } from "../../Services/Doctor/dashboardKpi.js";
+import { getMonthlyPatientsData } from "../../Services/Doctor/graphData.js";
 import { successResponse ,errorResponse } from "../../Traits/response.js";
+
 export const kpiData =async (req, res)=>{
     try {
         const doctorId = parseInt(req.params.id);
@@ -10,4 +12,14 @@ export const kpiData =async (req, res)=>{
         errorResponse(res,error,500)
         // res.status(500).json({ error: 'Internal server error' });
       }
+}
+
+export const graphsData = async (req, res) => {
+    try {
+        const data = await getMonthlyPatientsData()
+        res.json(data)
+    } catch (error) {
+        console.error('graphsData error:', error);
+        errorResponse(res,error,500)
+    }
 }
