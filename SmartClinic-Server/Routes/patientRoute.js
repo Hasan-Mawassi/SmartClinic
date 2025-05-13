5e219d041c0d81c9f435075ad89534095e280621
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 01:52:12 +0300

    add get prescription to patient route

--- SmartClinic-Server/Routes/patientRoute.js
+++ SmartClinic-Server/Routes/patientRoute.js
@@ -3,4 +3,5 @@ import * as patientController from "../Controllers/Patient/patientController.js"
 const patientRouter = Router();
 
 patientRouter.get('/patient/getinfo', patientController.getPatientInfo)
+patientRouter.get('/patient/getperscription', patientController.getPatientPrescription)
 export default patientRouter;
