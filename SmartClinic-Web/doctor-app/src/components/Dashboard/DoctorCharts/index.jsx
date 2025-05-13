7991e26738a5c755f1ca74e931457e855b41df4a
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 22:21:13 +0300

    SmartClinic-Web\doctor-app\src\components\Dashboard\DoctorCharts\index.jsx

--- SmartClinic-Web/doctor-app/src/components/Dashboard/DoctorCharts/index.jsx
+++ SmartClinic-Web/doctor-app/src/components/Dashboard/DoctorCharts/index.jsx
@@ -6,26 +6,26 @@ import {
   LineChart,
 } from '@mui/x-charts';
 
-const patientsData = [
-  { month: 'Jan', patients: 24 },
-  { month: 'Feb', patients: 18 },
-  { month: 'Mar', patients: 32 },
-  { month: 'Apr', patients: 22 },
-];
+// const patientsData = [
+//   { month: 'Jan', patients: 24 },
+//   { month: 'Feb', patients: 18 },
+//   { month: 'Mar', patients: 32 },
+//   { month: 'Apr', patients: 22 },
+// ];
 
-const genderData = [
-  { label: 'Male', value: 60 },
-  { label: 'Female', value: 40 },
-];
+// const genderData = [
+//   { label: 'Male', value: 60 },
+//   { label: 'Female', value: 40 },
+// ];
 
-const ageData = [
-  { ageRange:10 , count: 12 },
-  { ageRange:20 , count: 22 },
-  { ageRange:30 , count: 18 },
-  { ageRange: 50, count: 8 },
-];
+// const ageData = [
+//   { ageRange:10 , count: 12 },
+//   { ageRange:20 , count: 22 },
+//   { ageRange:30 , count: 18 },
+//   { ageRange: 50, count: 8 },
+// ];
 
-const DoctorDashboardCharts = () => {
+const DoctorDashboardCharts = ({patientsData,genderData,ageData}) => {
   return (
     <Box  spacing={3} mt={1} p={0} display={'flex'} gap={3} flexWrap={'wrap'} flexGrow={1}>
       
