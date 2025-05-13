aab85f423cf4996f86623ab89b6da61e0a95900d
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 22:14:35 +0300

    add graphs data reducer to store

--- SmartClinic-Web/doctor-app/src/redux/store.js
+++ SmartClinic-Web/doctor-app/src/redux/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import { createLogger } from "redux-logger";
 import doctorInfoReducer from './Slices/doctorInfoSlice'
 import kpiDataReducer from './Slices/kpiSlice.js'
+import grapsDataRedicer from './Slices/graphsDataSlice.js'
 
 const logger = createLogger();
 
 export const store = configureStore({
   reducer: {
     doctorInfo:doctorInfoReducer,
-    kpisData:kpiDataReducer
+    kpisData:kpiDataReducer,
+    grapsData:grapsDataRedicer
   },
   middleware: (defaultMiddleware) => defaultMiddleware().concat(logger),
 })
