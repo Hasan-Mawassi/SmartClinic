5a3de1be39598b52183dac4afdf8754573b4ecb7
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 21:15:35 +0300

    get kpi data from store

--- SmartClinic-Web/doctor-app/src/pages/Dashboard/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/Dashboard/index.jsx
@@ -1,5 +1,5 @@
 // pages/Dashboard.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Box, CircularProgress } from '@mui/material';
 import KpiCard from '../../components/Basic/KPICard';
 import WelcomeSection from '../../components/Dashboard/Welcome';
@@ -10,47 +10,25 @@ import DoneAllIcon from '@mui/icons-material/DoneAll';
 import DoctorDashboardCharts from '../../components/Dashboard/DoctorCharts';
 import AppointmentTable from '../../components/Dashboard/AppointmentTable';
 import { useSelector } from 'react-redux'
-import { request } from '../../utils/request';
+// import { request } from '../../utils/request';
+import { useKpiData } from '../../hooks/dashboard/useKpiData.js';
+
 const Dashboard = () => {
-  const [kpis, setKpis] = useState(null);
+  
   const doctor = useSelector((state) => state.doctorInfo)
   const doctorId = doctor.id;
   const doctorName = doctor.name;
 
-  const getKpiData = async (doctorId)=>{
-    try {
-        const response = await request({
-          method: "GET",
-          route: `/doctor/getkpi/${doctorId}`,
-          auth: true, // if the endpoint is protected
-        });
-    
-        if (response.error) {
-          console.error("Error fetching KPI data:", response.message);
-          return null;
-        }
-        
-        console.log(response.data) ; // adjust based on your actual backend structure
-      } catch (err) {
-        console.error("Unexpected error:", err);
-        return null;
-      }
-    };
+ const { loading } = useKpiData(doctorId);
+const kpiData = useSelector((state) => state.kpisData);
   
 
   useEffect(() => {
-     getKpiData(doctorId)
-    setTimeout(() => {
-      setKpis({
-        totalPatients: 124,
-        upcomingAppointments: 8,
-        pendingReports: 5,
-        completedConsultations: 42
-      });
-    }, 800);
+  
+   
   }, []);
 
-  if (!kpis) {
+  if (loading) {
     return (
       <Box display="flex" justifyContent="center" mt={5}>
         <CircularProgress />
@@ -68,28 +46,28 @@ const Dashboard = () => {
     <Box  sx={{flexGrow: 1, minWidth: 140 }}  size={{ xs: 12, sm: 6 ,md:3}}>
       <KpiCard
         title="Total Patients"
-        value={kpis.totalPatients}
+        value={kpiData.totalPatients}
         icon={<PeopleIcon fontSize="large" color="primary" />}
       />
     </Box>
     <Box sx={{flexGrow: 1, minWidth: 140 }} size={{ xs: 12, sm: 6,md:3 }}>
       <KpiCard
         title="Upcoming Appointments"
-        value={kpis.upcomingAppointments}
+        value={kpiData.upcomingAppointments}
         icon={<EventIcon fontSize="large" color="secondary" />}
       />
     </Box>
     <Box sx={{flexGrow: 1, minWidth: 140 }} size={{ xs: 12, sm: 6,md:3 }}>
       <KpiCard
         title="Pending Reports"
-        value={kpis.pendingReports}
+        value={kpiData.pendingReports}
         icon={<ReportIcon fontSize="large" sx={{ color: '#fb8c00' }} />}
       />
     </Box>
     <Box sx={{flexGrow: 1, minWidth: 140 }} size={{ xs: 12, sm: 6,md:4 }}>
       <KpiCard
         title="Completed This Month"
-        value={kpis.completedConsultations}
+        value={kpiData.completedToday}
         icon={<DoneAllIcon fontSize="large" sx={{ color: '#4caf50' }} />}
       />
     </Box>
