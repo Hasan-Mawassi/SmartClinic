c060898e4277a287300c93919fc2405cbf9dcc3e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 18:45:51 +0300

    add kpis slice to store

--- SmartClinic-Web/doctor-app/src/redux/store.js
+++ SmartClinic-Web/doctor-app/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { createLogger } from "redux-logger";
 import doctorInfoReducer from './Slices/doctorInfoSlice'
+import kpiDataReducer from './Slices/kpiSlice.js'
 
 const logger = createLogger();
 
 export const store = configureStore({
   reducer: {
     doctorInfo:doctorInfoReducer,
+    kpisData:kpiDataReducer
   },
   middleware: (defaultMiddleware) => defaultMiddleware().concat(logger),
 })
