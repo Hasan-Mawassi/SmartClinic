2ed2a70eb0ffc53fe658a7eb1d9b1af47581f682
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 13:33:28 +0300

    create request function

---
+++ SmartClinic-Web/doctor-app/src/utils/request.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+axios.defaults.baseURL =  import.meta.env.VITE_BASE_URL
+
+export const request = async ({
+  method,
+  route,
+  body,
+  auth = false,
+  optimistic,
+  rollback,
+}) => {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  if (auth) {
+    headers.Authorization = `Bearer ${localStorage.access_token}`;
+  }
+
+  try {
+    if (optimistic) {
+      optimistic(body);
+    }
+
+    const response = await axios.request({
+      method, 
+      headers,
+      url: route,
+      data: body,
+    });
+
+    return response.data;
+  } catch (error) {
+    if (rollback) {
+      rollback();
+    }
+
+    return {
+      error: true,
+      message: error.message,
+    };
+  }
+};
