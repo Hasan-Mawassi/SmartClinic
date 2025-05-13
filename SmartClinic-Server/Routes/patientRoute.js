84b8035b9c98709088e1cc7d6f57365951aced39
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 01:05:28 +0300

    add patient get info end point

--- SmartClinic-Server/Routes/patientRoute.js
+++ SmartClinic-Server/Routes/patientRoute.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-
+import * as patientController from "../Controllers/Patient/patientController.js"
 const patientRouter = Router();
 
-
+patientRouter.get('/patient/getinfo', patientController.getPatientInfo)
 export default patientRouter;
