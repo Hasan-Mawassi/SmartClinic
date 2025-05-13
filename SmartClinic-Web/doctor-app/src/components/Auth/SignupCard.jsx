f8e3ccfe30b44d21a87eb80b0ec6d3b11691cf6c
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 15:10:13 +0300

    update sign up card

--- SmartClinic-Web/doctor-app/src/components/Auth/SignupCard.jsx
+++ SmartClinic-Web/doctor-app/src/components/Auth/SignupCard.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
-import InputField from '../../components/Basic/inputField';
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
-import PersonIcon from '@mui/icons-material/Person';
-import CustomCard from '../../components/Basic/authCard';
-import CustomButton from '../../components/Basic/Button';
-import { Link } from '@mui/material';
-const SignupCard = ({linkClick , SignupClick}) => {
-    return (
-        <CustomCard title='Register'>
-        <InputField inputLabel='Username' placeholder="Enter your username" type="text" variant="outlined" fullWidth={true} icon={PersonIcon} />
-        <InputField inputLabel='Email' placeholder="Enter your email" type="email" variant="outlined" fullWidth={true} icon={EmailIcon} />
-        <InputField inputLabel='Password' placeholder="Enter your password" type="password" variant="outlined" fullWidth={true} icon={LockIcon} />
-       
-        <CustomButton
-          className='register-button'
-          label="Register"
-          onClick={SignupClick}
-          loading={false}
-          />
-         <div className='link'>
-         Already have an account?{' '}
-          <Link
+import React, { useState } from "react";
+import InputField from "../../components/Basic/inputField";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import PersonIcon from "@mui/icons-material/Person";
+import CustomCard from "../../components/Basic/authCard";
+import CustomButton from "../../components/Basic/Button";
+import { Link } from "@mui/material";
+const SignupCard = ({ onSignup, onLinkClick, loading }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignupClick = () => {
+    onSignup({ username, email, password });
+  };
+  return (
+    <CustomCard title="Register">
+      <InputField
+        inputLabel="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter your username"
+        type="text"
+        variant="outlined"
+        fullWidth={true}
+        icon={PersonIcon}
+      />
+      <InputField
+        inputLabel="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+        type="email"
+        variant="outlined"
+        fullWidth={true}
+        icon={EmailIcon}
+      />
+      <InputField
+        inputLabel="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter your password"
+        type="password"
+        variant="outlined"
+        fullWidth={true}
+        icon={LockIcon}
+      />
+
+      <CustomButton
+        className="register-button"
+        label="Register"
+        onClick={handleSignupClick}
+        loading={loading}
+      />
+      <div className="link">
+        Already have an account?{" "}
+        <Link
           component="button"
           variant="body2"
-          onClick={linkClick}
-          sx={{ textDecoration: 'underline', cursor: 'pointer' }}
-          >
+          onClick={onLinkClick}
+          sx={{ textDecoration: "underline", cursor: "pointer" }}
+        >
           Login
-          </Link>
-         </div>
-     </CustomCard>
-    );
+        </Link>
+      </div>
+    </CustomCard>
+  );
 };
 
 export default SignupCard;
