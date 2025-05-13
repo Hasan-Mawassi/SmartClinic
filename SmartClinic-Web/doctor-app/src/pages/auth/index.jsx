e5ac6467fc829738df90bdf79b4aebc5511edaba
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 15:09:45 +0300

    add login url

--- SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/auth/index.jsx
@@ -2,12 +2,42 @@ import React, { useState } from 'react';
 import './style.css';
 import LoginCard from '../../components/Auth/LoginCard';
 import SignupCard from '../../components/Auth/SignupCard';
+import { request } from '../../utils/request';
+import { useNavigate } from 'react-router-dom';
 const Auth = () => {
+    const navigate = useNavigate();
     const [isLogin, setIsLogin] = useState(true);
+    const [loading, setLoading] = useState(false)
+    const handleLogin = async ({ email, password }) => {
+        try {
+          setLoading(true);
+          const response = await request({
+            method: "POST",
+            route: `/auth/doctor/login`,
+            body: { email, password },
+            auth: false,
+          })
+          if(response.success){
+            localStorage.setItem('access_token',response.data.token )
+            navigate('/doctor/dashboard')
+          }
+          console.log(response)
+        } catch (error) {
+          // show error
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      const handleLinkClick = () => {
+        setIsLogin(false)
+      }
     return (
         <div className='auth-body'>
         <div className='top-right-circle'></div>
-          {isLogin ? (<LoginCard linkClick={() => setIsLogin(false)} loginClick={() => console.log('login clicked')}/>) 
+          {isLogin ? (<LoginCard  onLogin={handleLogin}
+      onLinkClick={handleLinkClick}
+      loading={loading}  />) 
             : (
                <SignupCard linkClick={() => setIsLogin(true)} SignupClick={() => console.log('signup clicked')}/>
             )  
