e5a5932921c81c81bf1cd3f6982df3d9baf0f3cb
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 01:40:43 +0300

    create get prescriptions data for patient

--- SmartClinic-Server/Models/Perscription.js
+++ SmartClinic-Server/Models/Perscription.js
@@ -1,5 +1,30 @@
 import prisma from "../lib/prisma.js";
 
 export class Perscription {
+
+    
+ static async getPrescriptions(patientId) {
+    const prescriptions = await prisma.$queryRaw`
+      SELECT 
+        DATE("createdAt") AS "date",
+        JSON_AGG(
+          JSON_BUILD_OBJECT(
+            'id', p.id,
+            'doctorId', p."doctorId",
+            'medicineName', p."medicineName",
+            'duration', p.duration,
+            'frequency', p.frequency,
+            'quantity', p.quantity,
+            'createdAt', p."createdAt"
+          )
+        ) AS prescriptions
+      FROM "Prescription" p
+      WHERE p."patientId" = ${patientId}
+      GROUP BY DATE(p."createdAt")
+      ORDER BY DATE(p."createdAt") DESC;
+    `;
+
+    return prescriptions;
+  }
     
 }
