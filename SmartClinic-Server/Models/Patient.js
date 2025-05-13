8dbcb7cf1609c00867d27dda83b62672538cc1c8
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 00:52:26 +0300

    create find unique patien

--- SmartClinic-Server/Models/Patient.js
+++ SmartClinic-Server/Models/Patient.js
@@ -2,5 +2,11 @@ import prisma from "../lib/prisma.js";
 
 export class Patient {
 
-    
+    static async findPatientById(id){
+        return await  prisma.patient.findUnique({
+      where: {
+        id,
+      },
+    });
+    }
 }
