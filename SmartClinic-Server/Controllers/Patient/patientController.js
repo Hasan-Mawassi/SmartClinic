0ae3b110ef23875a11f2d003771b17ba811fcc63
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 00:57:21 +0300

    initiate get patient function

--- SmartClinic-Server/Controllers/Patient/patientController.js
+++ SmartClinic-Server/Controllers/Patient/patientController.js
@@ -1,1 +1,14 @@
-//
+import { Patient } from "../../Models/Patient";
+
+
+export const  getPatientInfo =  async (req , res)=>{
+    try {
+         const {id} = req.params;
+        const patient = await Patient.findPatientById(id);
+        res.json(patient)
+    } catch (error) {
+        res.send(error)
+    }
+       
+
+}
