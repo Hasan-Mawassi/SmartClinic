14e4a63cd38a0e38f59630b33f3b416798e8065d
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 16:36:17 +0300

    update login card to handle erros

--- SmartClinic-Web/doctor-app/src/components/Auth/LoginCard.jsx
+++ SmartClinic-Web/doctor-app/src/components/Auth/LoginCard.jsx
@@ -4,8 +4,8 @@ import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
 import CustomCard from '../../components/Basic/authCard';
 import CustomButton from '../../components/Basic/Button';
-import { Link } from '@mui/material';
-const LoginCard = ({ onLogin, onLinkClick, loading }) => {
+import { Link , Typography} from '@mui/material';
+const LoginCard = ({ onLogin, onLinkClick, loading ,message, showMessage}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const handleLoginClick = () => {
@@ -13,6 +13,15 @@ const LoginCard = ({ onLogin, onLinkClick, loading }) => {
     };
     return (
         <CustomCard title='Login'>
+              {showMessage && (
+                    <Typography
+                      variant="body2"
+                      color="error"
+                      sx={{  textAlign: "center" }}
+                    >
+                      {message}
+                    </Typography>
+                  )}
               <InputField inputLabel='Email' placeholder="Enter your email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} variant="outlined" fullWidth={true} icon={EmailIcon} />
               <InputField inputLabel='Password' placeholder="Enter your password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} variant="outlined" fullWidth={true} icon={LockIcon} />
              
