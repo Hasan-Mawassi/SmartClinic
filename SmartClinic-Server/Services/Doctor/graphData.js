742144b042a2db638c4ec630717a2fc7db10c571
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-13 11:01:07 +0300

    add patient age analysis function

--- SmartClinic-Server/Services/Doctor/graphData.js
+++ SmartClinic-Server/Services/Doctor/graphData.js
@@ -32,9 +32,51 @@ export const getGenderStats = async () => {
     
     const femaleCount = await prisma.patient.count({ where: { gender: 0 } });
     const maleCount = await  prisma.patient.count({ where: { gender: 1 } });
-    
+
     return {
       female: femaleCount,
       male: maleCount,
     };
   };
+
+ export const getPatientAgeAnalysis = async (doctorId) =>{
+    // Fetch all appointments for the doctor with patient info
+    const appointments = await prisma.appointment.findMany({
+      where: { doctorId },
+      include: {
+        patient: true,
+      },
+    });
+  
+    // Extract unique patients using a Map
+    const uniquePatients = new Map();
+  
+    for (const appt of appointments) {
+      const patient = appt.patient;
+      console.log(patient)
+      if (!uniquePatients.has(patient.id)) {
+        uniquePatients.set(patient.id, patient);
+      }
+    }
+  
+    //  Analyze age distribution
+    const now = new Date();
+    const ageGroups = {};
+  
+    for (const patient of uniquePatients.values()) {
+      const birthDate = new Date(patient.date_of_birth);
+      const age = now.getFullYear() - birthDate.getFullYear();
+        
+      // 5-year range
+      const ageRange = Math.round(age / 5) * 5
+      ageGroups[ageRange] = (ageGroups[ageRange] || 0) + 1;
+    }
+  
+    //  Format output
+    const ageData = Object.entries(ageGroups).map(([ageRange, count]) => ({
+      ageRange: parseInt(ageRange),
+      count,
+    }));
+  
+    return ageData;
+  }
