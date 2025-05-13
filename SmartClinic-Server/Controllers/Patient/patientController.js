42683591d937e446cb7c07909c43e795d5ad4ecd
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 01:18:56 +0300

    add response trait

--- SmartClinic-Server/Controllers/Patient/patientController.js
+++ SmartClinic-Server/Controllers/Patient/patientController.js
@@ -1,14 +1,16 @@
-import { Patient } from "../../Models/Patient";
+import { Patient } from "../../Models/Patient.js";
+import { successResponse ,errorResponse } from "../../Traits/response.js";
 
 
 export const  getPatientInfo =  async (req , res)=>{
-    try {
-         const {id} = req.params;
-        const patient = await Patient.findPatientById(id);
-        res.json(patient)
-    } catch (error) {
-        res.send(error)
-    }
+   try {
+    const { id } = req.body;
+    const patient = await Patient.findPatientById(id); 
+     successResponse(res, patient, 'Patient info', 200);
+  } catch (error) {
+    console.error('Error getting patient info:', error);
+    errorResponse(res,error,500)
+  }
        
 
 }
