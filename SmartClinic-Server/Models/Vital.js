de7c8f8c67a4a2fdbc68a2f9a80efb010bdcca56
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 11:45:53 +0300

    add create vital to vital class

--- SmartClinic-Server/Models/Vital.js
+++ SmartClinic-Server/Models/Vital.js
@@ -2,4 +2,29 @@ import prisma from "../lib/prisma.js";
 
 export class Vital {
     
+  async createVital({
+    patientId,
+    doctorId,
+    healthPercent,
+    heartRate,
+    bloodPressure,
+    temperature,
+    bloodGlucose,
+  }) {
+    return await prisma.vital.create({
+      data: {
+        patientId,
+        doctorId,
+        healthPercent,
+        heartRate,
+        bloodPressure,
+        temperature,
+        bloodGlucose,
+      },
+    });
+  }
+
+
+
+
 }
