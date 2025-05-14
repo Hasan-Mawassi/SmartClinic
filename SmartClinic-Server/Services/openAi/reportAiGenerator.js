6c0b0bf9d4fa2b2f204697b1ec32d4760d8d4b38
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 16:20:59 +0300

    add open ai api function

--- SmartClinic-Server/Services/openAi/reportAiGenerator.js
+++ SmartClinic-Server/Services/openAi/reportAiGenerator.js
@@ -1,3 +1,29 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const client  = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+ export const  generateOverviewReport =async ( report) =>{
+const reportPrompt = generatePatientPrompt(report) ;
+  const aiResponse = await client.responses.create({
+    model:  "gpt-4.1",
+    input: [
+      { role: 'developer', content: 'You are a helpful and professional medical assistant that help doctor generate reports.' },
+      { role: 'user', content: reportPrompt },
+    ],
+    temperature: 0.7,
+  });
+  return aiResponse.output_text;
+}
+
+
+
+
+
+
+
+
+
 export  const generatePatientPrompt = (report) =>{
   return `
 Patient Report Summary
@@ -16,6 +42,6 @@ ${report.recentVital.map(t => `- (${t.date}) heart rate ${t.heartRate} bood pres
 Recent Medications:
 ${report.recentMedications.map(m => `- ${m.name} (${m.dosage}) start date ${m.startDate} `).join("\n") || "None"}
 
-Please write a concise and professional summary of this patient’s current medical condition, including any important risks, recommendations, and follow-ups to give an overview for the doctor.
+Please write a concise and professional summary of this patient’s current medical condition, including any important risks, make as much as posibile brief report.
   `;
 }
