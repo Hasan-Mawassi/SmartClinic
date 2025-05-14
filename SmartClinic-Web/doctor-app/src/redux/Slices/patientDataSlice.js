a3bc46b5641a1b987c0eb2af4daa74f41f025363
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 17:09:53 +0300

    add set patientId to reducer

--- SmartClinic-Web/doctor-app/src/redux/Slices/patientDataSlice.js
+++ SmartClinic-Web/doctor-app/src/redux/Slices/patientDataSlice.js
@@ -8,12 +8,14 @@ export const patientDataSlice = createSlice({
   name: 'patientData',
   initialState,
   reducers: {
-   
+   setPatientId: (state, action) => {
+      state.patientId = action.payload
+    },
     
   },
 })
 
 
-export const { } = patientDataSlice.actions
+export const {setPatientId  } = patientDataSlice.actions
 
 export default patientDataSlice.reducer
