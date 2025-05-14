d3aee0493638e4435527b7f947024bc48f0f3b7e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 22:32:11 +0300

    create ai report hook

---
+++ SmartClinic-Web/doctor-app/src/hooks/patient/useReport.js
@@ -0,0 +1,38 @@
+import {  useEffect } from 'react';
+
+import { request } from '../../utils/request.js';
+
+
+export const useAiReport = (patientId) => {
+
+ 
+
+  useEffect(() => {
+    const fetchAiReport = async () => {
+      try {
+        const response = await request({
+          method: 'POST',
+          route: `patient/doctor/ai/generatereport`,
+          body:{patientId},
+          auth: true,
+        });
+
+        if (!response.error) {
+         console.log(response)
+        } else {
+          console.error('Error fetching AI Report:', response.message);
+        }
+      } catch (err) {
+        console.error('Unexpected error fetching AI Report:', err);
+      } finally {
+        console.log("finish AI Report")
+      }
+    };
+
+    if (patientId) {
+      fetchAiReport();
+    }
+  }, [patientId]);
+
+  
+};
