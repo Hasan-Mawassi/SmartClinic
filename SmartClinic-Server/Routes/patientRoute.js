f7b5c82cb1921df9e7ff942521cd6f9ec4b66229
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 15:31:56 +0300

    create get report end point

--- SmartClinic-Server/Routes/patientRoute.js
+++ SmartClinic-Server/Routes/patientRoute.js
@@ -5,4 +5,5 @@ const patientRouter = Router();
 patientRouter.get('/patient/getinfo', patientController.getPatientInfo)
 patientRouter.get('/patient/getperscription', patientController.getPatientPrescription)
 patientRouter.get('/patient/getvital', patientController.getPatietnVital)
+patientRouter.get('/patient/getreport', patientController.generatePatientReport)
 export default patientRouter;
