8e7bb7c96167c71879f0bd3e5880a263535e382d
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 22:14:22 +0300

    add vital data custom hook

---
+++ SmartClinic-Web/doctor-app/src/hooks/patient/useVitalData.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { request } from '../../utils/request.js';
+import { setVitalData } from '../../redux/Slices/patientDataSlice.js'; 
+
+export const usePatientVitalData = (patientId) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchVitalData = async () => {
+      try {
+        const response = await request({
+          method: 'GET',
+          route: `/patient/getvital?id=${patientId}`, 
+          auth: true,
+        });
+
+        if (!response.error) {
+          dispatch(setVitalData(response.data));
+        } else {
+          console.error('Error fetching vital data:', response.message);
+        }
+      } catch (err) {
+        console.error('Unexpected error fetching vital data:', err);
+      } finally {
+        console.log("Finished fetching vital data");
+      }
+    };
+
+    if (patientId) {
+      fetchVitalData();
+    }
+  }, [patientId, dispatch]);
+};
