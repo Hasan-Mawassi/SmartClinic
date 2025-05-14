dbafb012f12e455b6ea340830f881fd23ea0ce3b
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 17:26:32 +0300

    initiate patient info custom hook

---
+++ SmartClinic-Web/doctor-app/src/hooks/patient/usePatientInfo.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { request } from '../../utils/request.js';
+
+
+export const usePatientInfo = (patientId) => {
+
+
+  const [patientLoading, setPatientLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPatientInfo = async () => {
+      try {
+        const response = await request({
+          method: 'GET',
+          route: `/patient/info/${patientId}`,
+          auth: true,
+        });
+
+        
+          console.error('Error fetching patient info:', response.message);
+       
+      } catch (err) {
+        console.error('Unexpected error:', err);
+      } finally {
+        setPatientLoading(false);
+      }
+    };
+
+    if (patientId) {
+      fetchPatientInfo();
+    }
+  }, [patientId]);
+
+  return { patientLoading };
+}
