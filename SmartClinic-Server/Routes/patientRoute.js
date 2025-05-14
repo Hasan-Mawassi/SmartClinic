6bf2c0a60fe58eddbfda4b9e83388bd40e0a4d1f
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 18:02:10 +0300

    change to post due to browser prevent get body req

--- SmartClinic-Server/Routes/patientRoute.js
+++ SmartClinic-Server/Routes/patientRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as patientController from "../Controllers/Patient/patientController.js"
 const patientRouter = Router();
 
-patientRouter.get('/patient/getinfo', patientController.getPatientInfo)
+patientRouter.post('/patient/getinfo', patientController.getPatientInfo)
 patientRouter.get('/patient/getperscription', patientController.getPatientPrescription)
 patientRouter.get('/patient/getvital', patientController.getPatietnVital)
 patientRouter.get('/patient/getreport', patientController.generatePatientReport)
