39e07b186f6923866c4335bfa915f9ea8771986b
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-14 15:35:43 +0300

    fix array map bugs

--- SmartClinic-Server/Services/Doctor/patientReport.js
+++ SmartClinic-Server/Services/Doctor/patientReport.js
@@ -2,23 +2,35 @@ import { Patient } from "../../Models/Patient.js";
 
 
 export async function getPatientReport(patientId) {
-  const report = Patient.getPatientReport(patientId);
+  const report = await Patient.getPatientReport(patientId);
 
   if (!report) throw new Error("Patient not found");
 
   return {
-    name: report.name,
-    allergies: report?.allergies.map(a => a.name),
-    chronicDiseases: report?.diseases.map(d => d.name),
-    recentVital: report?.vitals.map(t => ({
-      date: t.createdAt,
-      heartRate: t.heartRate,
-      bloodPressure: t.bloodPressure
-    })),
-    recentMedications: report?.medications.map(m => ({
-      name: m.medicineName,
-      dosage: m.quantity,
-      startDate: m.createdAt,
-    })),
-  };
+  name: report.name || 'Unknown Patient',
+
+  allergies: Array.isArray(report?.Allergy) && report.Allergy.length > 0
+    ? report.Allergy.map(a => a.name)
+    : ['None'],
+
+  chronicDiseases: Array.isArray(report?.diseases) && report.diseases.length > 0
+    ? report.diseases.map(d => d.name)
+    : ['None'],
+
+  recentVital: Array.isArray(report?.vitals) && report.vitals.length > 0
+    ? report.vitals.map(t => ({
+        date: t.createdAt,
+        heartRate: t.heartRate,
+        bloodPressure: t.bloodPressure
+      }))
+    : [],
+
+  recentMedications: Array.isArray(report?.prescriptions) && report.prescriptions.length > 0
+    ? report.prescriptions.map(m => ({
+        name: m.medicineName,
+        dosage: m.quantity,
+        startDate: m.createdAt
+      }))
+    : []
+};
 }
