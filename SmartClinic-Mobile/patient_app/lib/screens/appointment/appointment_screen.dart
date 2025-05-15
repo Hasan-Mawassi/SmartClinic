f2879f6b2c32ee47b6e6e9f9934ab3653c151f16
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-15 17:45:51 +0300

    fex card over flow

--- SmartClinic-Mobile/patient_app/lib/screens/appointment/appointment_screen.dart
+++ SmartClinic-Mobile/patient_app/lib/screens/appointment/appointment_screen.dart
@@ -3,8 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:patient_app/widgets/appointment/appointment_detail_card.dart';
 import 'package:patient_app/constants/app_colors.dart';
 import 'package:patient_app/providers/appointment_provider.dart';
+
 class AppointmentScreen extends StatefulWidget {
-   final String patientId;
+  final String patientId;
 
   const AppointmentScreen({super.key, required this.patientId});
 
@@ -13,20 +14,23 @@ class AppointmentScreen extends StatefulWidget {
 }
 
 class _AppointmentScreenState extends State<AppointmentScreen> {
-   @override
+  @override
   void initState() {
-  super.initState();
-  WidgetsBinding.instance.addPostFrameCallback((_) {
-    Provider.of<AppointmentProvider>(context, listen: false)
-        .fetchAppointments(widget.patientId);
-  });
-}
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<AppointmentProvider>(
+        context,
+        listen: false,
+      ).fetchAppointments(widget.patientId);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(title: const Text("Appointment")),
-      body:  Consumer<AppointmentProvider>(
+      body: Consumer<AppointmentProvider>(
         builder: (context, provider, _) {
           if (provider.isLoading) {
             return const Center(child: CircularProgressIndicator());
@@ -37,20 +41,29 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
           }
 
           return ListView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
             itemCount: provider.appointments.length,
             itemBuilder: (context, index) {
               final appointment = provider.appointments[index];
-              final date = "${appointment.dateTime.day.toString().padLeft(2, '0')}-${appointment.dateTime.month.toString().padLeft(2, '0')}-${appointment.dateTime.year}";
-              final time = TimeOfDay.fromDateTime(appointment.dateTime).format(context);
+              final date =
+                  "${appointment.dateTime.day.toString().padLeft(2, '0')}-${appointment.dateTime.month.toString().padLeft(2, '0')}-${appointment.dateTime.year}";
+              final time = TimeOfDay.fromDateTime(
+                appointment.dateTime,
+              ).format(context);
 
-              return AppointmentTile(
-                photoUrl: appointment.doctorImage,
-                doctorName: appointment.doctorName,
-                date: date,
-                time: time,
-                onCancel: () {
-                  print("Appointment ${appointment.id} canceled");
-                },
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16.0,
+                ), // Space between items
+                child: AppointmentTile(
+                  photoUrl: appointment.doctorImage,
+                  doctorName: appointment.doctorName,
+                  date: date,
+                  time: time,
+                  onCancel: () {
+                    print("Appointment ${appointment.id} canceled");
+                  },
+                ),
               );
             },
           );
