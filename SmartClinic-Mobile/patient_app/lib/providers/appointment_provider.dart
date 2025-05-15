3596eab7ec4b7fde0407484897f3c3dcd6ce71c4
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-15 16:29:09 +0300

    fix notifyer error"

--- SmartClinic-Mobile/patient_app/lib/providers/appointment_provider.dart
+++ SmartClinic-Mobile/patient_app/lib/providers/appointment_provider.dart
@@ -3,21 +3,22 @@ import 'package:patient_app/utils/request.dart';
 import '../models/appointment.dart';
 
 class AppointmentProvider with ChangeNotifier {
+  final requestClient = RequestClient().dio;
+
   List<Appointment> _appointments = [];
   bool _isLoading = false;
 
   List<Appointment> get appointments => _appointments;
   bool get isLoading => _isLoading;
 
-  final requestClient = RequestClient().dio;
-
   Future<void> fetchAppointments(String patientId) async {
-    _isLoading = true;
-    notifyListeners();
-
     try {
-      final response = await requestClient.get("patient/getAppointments?id=$patientId");
+      _isLoading = true;
+      notifyListeners();
+      final response = await requestClient.get("patient/getAppointments?id=2");
+
       final data = response.data as List;
+      
       _appointments = data.map((e) => Appointment.fromJson(e)).toList();
     } catch (e) {
       print("Error fetching appointments: $e");
