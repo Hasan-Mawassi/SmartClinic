db49a77a78736d2e9913e77dbb50bf5c8f4d5c84
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-15 15:43:31 +0300

    change appointments screen to state full widget

--- SmartClinic-Mobile/patient_app/lib/screens/appointment/appointment_screen.dart
+++ SmartClinic-Mobile/patient_app/lib/screens/appointment/appointment_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:patient_app/widgets/appointment/appointment_detail_card.dart';
 import 'package:patient_app/constants/app_colors.dart';
-class AppointmentScreen extends StatelessWidget {
+
+class AppointmentScreen extends StatefulWidget {
   const AppointmentScreen({super.key});
 
+  @override
+  State<AppointmentScreen> createState() => _AppointmentScreenState();
+}
+
+class _AppointmentScreenState extends State<AppointmentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
