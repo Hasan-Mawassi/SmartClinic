464486b6605b596b771f7f06d18b151059237372
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-17 17:36:35 +0300

    add get available slots tool

--- SmartClinic-Server/LangChain/tools.js
+++ SmartClinic-Server/LangChain/tools.js
@@ -13,20 +13,27 @@ const textSchema = z.object({
   text: z.string().describe("text to extract date from it"),
 });
 
-const get_date_from_text = tool(
+const get_available_slots = tool(
   async ({ text }) => {
     const date = await extractDateFromText(text);
-    return date
+    const slots = await generateAvailableSlots({
+                  doctorId: 1,
+                  dateISO:date,
+                  startTime: "8:00",
+                  endTime:  "18:00",
+                  slotDurationMinutes: 30
+                });
+    return `available slots ${slots}` 
   },
   {
-    name: "get_date_from_text",
+    name: "get_available_slots",
     description: `Today is ${new Date().toISOString().split('T')[0]}. Extract the date from the message.
                      Return it in ISO 8601 format (e.g., 2025-05-05). Reply ONLY with the date.`,
     schema: textSchema,
   }
 );
  
-const llmWithTools = llm.bindTools([get_date_from_text]);
+const llmWithTools = llm.bindTools([get_available_slots]);
 
 const initialResponse  = await llmWithTools.invoke("What are the date next tuesday");
 const toolCall = initialResponse.tool_calls?.[0];
@@ -35,8 +42,8 @@ if (toolCall) {
   // STEP 2: Run the requested tool manually
   let toolOutput = "";
 
-  if (toolCall.name === "get_date_from_text") {
-    toolOutput = await get_date_from_text.invoke(toolCall.args);
+  if (toolCall.name === "get_available_slots") {
+    toolOutput = await get_available_slots.invoke(toolCall.args);
   }
   const finalResponse = await llmWithTools.invoke([
       initialResponse,
