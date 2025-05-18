8c8926bb19ab75341e68dc6776e66fc8699b76cb
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-18 14:51:09 +0300

    update fuction calling

--- SmartClinic-Server/LangChain/handleChatAi.js
+++ SmartClinic-Server/LangChain/handleChatAi.js
@@ -1,19 +1,25 @@
+import { gpt } from "./chatModels.js";
+import { ToolMessage } from "@langchain/core/messages"; 
+import { get_available_slots, book_appointemnt } from "./tools.js";
+
 
 
 
 export const handleChatWithAI = async ( userId,userInput,doctor) => {
-    const llmWithTools = gpt.bindTools([ get_available_slots(doctor),
-    book_appointemnt(userId, doctor),]);
+    const tools = {
+    getSlots: get_available_slots(doctor),
+    bookSlot: book_appointemnt(userId, doctor),
+  };
+  const llmWithTools = gpt.bindTools([tools.getSlots, tools.bookSlot]);
   const initialResponse = await llmWithTools.invoke(userInput);
   const toolCall = initialResponse.tool_calls?.[0];
-
   if (toolCall) {
     let toolOutput = "";
 
     if (toolCall.name === "get_available_slots") {
-      toolOutput = await  get_available_slots(doctor).invoke(toolCall.args);
+      toolOutput = await  tools.getSlots.invoke(toolCall.args);
     } else if (toolCall.name === "book_appointemnt") {
-      toolOutput = await book_appointemnt(userId, doctor).invoke(toolCall.args);
+      toolOutput = await tools.bookSlot.invoke(toolCall.args);
     }
 
     const finalResponse = await llmWithTools.invoke([
