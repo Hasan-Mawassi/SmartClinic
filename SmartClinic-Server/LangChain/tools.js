3f4bc6a5351765280cdd0de84d6b612956e1f044
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-18 10:29:49 +0300

    remove handle chat with ai function

--- SmartClinic-Server/LangChain/tools.js
+++ SmartClinic-Server/LangChain/tools.js
@@ -2,9 +2,9 @@ import { tool } from "@langchain/core/tools";
 import { gpt } from "./chatModels.js";
 import { z } from "zod";
 import { ToolMessage } from "@langchain/core/messages"; 
-import { extractDateFromText, bookAppointmentByIndex, generateAppointmentList } from '../Services/openAi/openAiService.js';
+import {  bookAppointmentByIndex } from '../Services/openAi/openAiService.js';
 import { generateAvailableSlots } from '../Services/openAi/slots.js';
-
+import { extractDateFromText } from "./chatModelFunctions.js";
 let available = []; 
 
 const textSchema = z.object({
@@ -37,7 +37,7 @@ const bookSchema = z.object({
 
 });
 
-const book_appointemnt =(userId, doctor) => tool(
+export const book_appointemnt =(userId, doctor) => tool(
   async ({ index }) => {
     if (!available.length) {
       return "No available slots. Ask for available slots first.";
@@ -56,31 +56,3 @@ const book_appointemnt =(userId, doctor) => tool(
 
 
 
-export const handleChatWithAI = async ( userId,userInput,doctor) => {
-    const llmWithTools = gpt.bindTools([ get_available_slots(doctor),
-    book_appointemnt(userId, doctor),]);
-  const initialResponse = await llmWithTools.invoke(userInput);
-  const toolCall = initialResponse.tool_calls?.[0];
-
-  if (toolCall) {
-    let toolOutput = "";
-
-    if (toolCall.name === "get_available_slots") {
-      toolOutput = await  get_available_slots(doctor).invoke(toolCall.args);
-    } else if (toolCall.name === "book_appointemnt") {
-      toolOutput = await book_appointemnt(userId, doctor).invoke(toolCall.args);
-    }
-
-    const finalResponse = await llmWithTools.invoke([
-      initialResponse,
-      new ToolMessage({
-        tool_call_id: toolCall.id,
-        content: toolOutput,
-      }),
-    ]);
-
-    return finalResponse.content;
-  }
-
-  return initialResponse.content;
-};
