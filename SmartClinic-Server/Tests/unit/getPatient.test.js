7f364b6758abf5d859686052c295802e6aef5776
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-20 00:53:10 +0300

    add get patient test

---
+++ SmartClinic-Server/Tests/unit/getPatient.test.js
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import app from '../../app.js'
+
+describe('GET /api/v1/doctor/todaypatients', () => {
+  it('should return today\'s patients for the doctor', async () => {
+    const doctorId = 1;
+
+    const res = await request(app)
+      .get(`/api/v1/doctor/todaypatients?doctorId=${doctorId}`)
+      // .set('Authorization', `Bearer ${token}`) 
+      .expect(200);
+
+    expect(res.body).toHaveProperty('success', true);
+    expect(res.body).toHaveProperty('message', 'Get upcomming Appointmets succesfully');
+    expect(res.body).toHaveProperty('data');
+    expect(Array.isArray(res.body.data)).toBe(true);
+  });
+});
