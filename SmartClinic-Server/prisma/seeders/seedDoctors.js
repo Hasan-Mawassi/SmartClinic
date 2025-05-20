c24dfed1a609ef71b004daf139fc4e989714e7c0
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-20 16:08:41 +0300

    add doctor seeders

---
+++ SmartClinic-Server/prisma/seeders/seedDoctors.js
@@ -0,0 +1,9 @@
+import prisma from '../../lib/prisma.js';
+import { doctorFactory } from '../factories/doctorFactory.js';
+
+export async function seedDoctors(count = 10) {
+  for (let i = 0; i < count; i++) {
+    const doctorData = doctorFactory();
+    await prisma.doctor.create({ data: doctorData });
+  }
+}
