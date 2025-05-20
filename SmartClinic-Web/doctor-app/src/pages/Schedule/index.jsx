823055550c41600c27c0b5cc1b8fd1a9d26f09ee
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-20 23:58:52 +0300

    add schedule modal

--- SmartClinic-Web/doctor-app/src/pages/Schedule/index.jsx
+++ SmartClinic-Web/doctor-app/src/pages/Schedule/index.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import ScheduleCard from '../../components/Schedule/ScheduleCard';
-import ScheduleTable from '../../components/Schedule/ScheduleTable';
-import { Box } from '@mui/material';
+import React from "react";
+import ScheduleCard from "../../components/Schedule/ScheduleCard";
+import ScheduleTable from "../../components/Schedule/ScheduleTable";
+import { Box } from "@mui/material";
+import CustomButton from "../../components/Basic/Button";
+import { useState } from "react";
 const Schedule = () => {
-    return (
-        <> 
-        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center',mt: 2 }}>
-            <ScheduleCard />
-        </Box>
-        <Box sx={{p:1 }}>
-        < ScheduleTable />
-        </Box>
-        </>
-    );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <Box
+        sx={{
+          p: 1,
+         
+          mt: 2,
+        }}
+      >
+        <CustomButton
+          label={"Add Schedule"}
+          onClick={() => setIsModalOpen(true)}
+          sx={{ width: "20%",  ml: 7,  }}
+        />
+      </Box>
+
+      <ScheduleCard open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <Box sx={{ p: 1 }}>
+        <ScheduleTable />
+      </Box>
+    </>
+  );
 };
 
 export default Schedule;
