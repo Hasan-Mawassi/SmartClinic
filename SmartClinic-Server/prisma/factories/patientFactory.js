acf74592eac8eabedb7c24cd2883988cef3cdc25
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-05-20 16:43:43 +0300

    add password to seeders

--- SmartClinic-Server/prisma/factories/patientFactory.js
+++ SmartClinic-Server/prisma/factories/patientFactory.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 export const patientFactory = () => ({
   name: faker.person.fullName(),
   email: faker.internet.email().toLowerCase(),
-  password: faker.internet.password(),
+  password:"123123",
   weight: parseFloat(faker.number.float({ min: 50, max: 100 }).toFixed(2)),
   gender: faker.number.int({ min: 0, max: 1 }),
   date_of_birth: faker.date.birthdate(),
